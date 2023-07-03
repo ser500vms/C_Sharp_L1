c7b7dbbec22ffe361a97f37185e1597a5e837e6d
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-03 21:24:54 +0300

    Создали программу которая выводит особое обращение только при вводе опредеоенного имени и обновили readme.md

---
+++ Example005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввежите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "сергей")
+{
+    Console.WriteLine("Ура, это же СЕРГЕЙ!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
